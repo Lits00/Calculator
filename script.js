c21acfad7762dd49ac68fc39036748ea54e5bd8f
Angelito Dionisio <litsdionisio@gmail.com>
2023-04-21 23:35:09 +0400

    Created calculator UI and operate function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let num1, operator, num2;
 
+// Arithmetic operators
 function add(a, b) {
     // console.log(a + b);
     return (a + b);
@@ -18,4 +19,17 @@ function multiply(a, b) {
 function divide(a, b) {
     // console.log(a / b);
     return (a / b);
+}
+
+// Initiator
+function operate(num1, operator, num2) {
+    if (operator === '+'){
+        add(num1, num2);       
+    } else if (operator === '-'){
+        subtract(num1, num2)
+    } else if (operator === '*'){
+        multiply(num1, num2)
+    } else if (operator === '/'){
+        divide(num1, num2)
+    }
 }
