e8542a2782c82fdb6e1a4ae78caa2204573988ae
Angelito Dionisio <litsdionisio@gmail.com>
2023-04-23 20:48:45 +0400

    Add keyboard press functions

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const deleteBtn = document.querySelector('.del');
 const dotBtn = document.querySelector('.dot');
 const equalsBtn = document.querySelector('.equals');
 
-// Operation
+// Formula
 function add(a, b) {
     return a + b;
 }
@@ -46,7 +46,7 @@ function operate(operator, a, b) {
     }
 }
 
-// Buttons func
+// Operations/Steps
 function count(){
     displayLength = currentDisplay.textContent.length;
 }
@@ -97,9 +97,10 @@ function addNumber(number){
 function addOperator(sign){
     if(operator !== null) solve();
     num1 = currentDisplay.textContent;
-    operator = sign;
+    operator = convertKey(sign);
     overallDisplay.textContent = `${num1} ${operator}`;
     resetDisplay();
+    solution = null;
 }
 
 function placeDot(){
@@ -126,17 +127,46 @@ function solve(){
     operator = null;
 }
 
-// Buttons
+function convertKey(inputKey){
+    if(inputKey === '*'){
+        return 'x';
+    } else if(inputKey === '/'){
+        return '÷';
+    } else {
+        return inputKey;
+    }
+}
+
+// Button click
 clearBtn.addEventListener('click', clearDisplay);
 deleteBtn.addEventListener('click', deleteInput);
 dotBtn.addEventListener('click', placeDot);
-equalsBtn.addEventListener('click', solve)
+equalsBtn.addEventListener('click', solve);
 
 
 numBtns.forEach(btn => {
-    btn.addEventListener('click', () => addNumber(btn.textContent))
+    btn.addEventListener('click', () => addNumber(btn.textContent));
 });
 
 operatorBtns.forEach(btn => {
-    btn.addEventListener('click', () => addOperator(btn.textContent))
+    btn.addEventListener('click', () => addOperator(btn.textContent));
+})
+
+// Keyboard press
+document.addEventListener('keydown', function(event) {
+    if(event.key >= 0 && event.key <=9){
+        addNumber(event.key);
+    } else if(event.key == '+' || event.key == '-' || event.key == '*' || event.key == '/'){
+        addOperator(event.key);
+    } else if(event.key === 'Escape'){
+        clearDisplay();
+    } else if(event.key === 'Backspace'){
+        deleteInput();
+    } else if(event.key === '.'){
+        placeDot();
+    } else if(event.key === '=' || event.key === 'Enter'){
+        solve();
+    } else {
+        return
+    }
 })
